9418005b45cb1c483d93bfda413403cb953125cc
BalintGergely <31952784+BalintGergely@users.noreply.github.com>
2018-12-05 10:30:29 +0100

    Fix for Odd-Even Sort
    Fixed Odd-Even sort to defect when it finds nothing to swap on the first iteration.

--- src/g/sort/vis/OddEvenSort.java
+++ src/g/sort/vis/OddEvenSort.java
@@ -6,11 +6,14 @@ public class OddEvenSort extends ConfigurableSorter{
 	public void sort(VisualArray vis,Sorter srt) {
 		int i = 1;
 		boolean b;
+		boolean d = true;
 		do{b = false;
 			while(i < vis.size){
 				b = (vis.compareAndSwap(i-1, i) > 0) | b;
 				i += 2;
 			}
+			b |= d;
+			d = false;
 			i = (i%2)+1;
 		}while(b);
 	}
